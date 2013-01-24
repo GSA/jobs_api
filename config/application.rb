d8606cad32dee017bcb7020b86a7808f6543fa0f
Loren Siebert <loren@siebert.org>
2013-01-24 10:53:43 -0800

    Update tire-contrib gem to get master branch with New Relic instrumentation

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'action_controller/railtie'
 require 'tire/rails/logger'
+require 'tire/new_relic/instrumentation'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
