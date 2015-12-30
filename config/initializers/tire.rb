70c7427534455fd41598ce159d29910868ca7c14
Nathan Smith <noremmie@gmail.com>
2015-12-30 09:23:37 -0800

    Pick up Elasticsearch uri from VCAP_SERVICES environment variable

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,3 +1,9 @@
-#Tire.configure { logger STDERR, level: 'debug' }
+service_credentials = JSON.parse(ENV['VCAP_SERVICES']) rescue nil
+if service_credentials
+  Tire.configure do
+    url service_credentials['elasticsearch-swarm-1.7.1'].first['credentials']['uri']
+  end
+end
+
 PositionOpening.create_search_index unless PositionOpening.search_index.exists?
 Geoname.create_search_index unless Geoname.search_index.exists?
