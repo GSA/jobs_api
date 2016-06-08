09c362401c7515072c682a6a468dd15126ce0e10
Nick Marden <nick@marden.org>
2016-06-08 16:07:37 -0400

    Allow elasticsearch.yml to configure Tire::Configuration.url

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,7 @@
-module Elasticsearch
-  es_config = (YAML.load_file("#{Rails.root}/config/elasticsearch.yml") || {})[Rails.env]
-  INDEX_NAME = es_config && es_config['index_name'].present? ? es_config['index_name'].freeze : "#{Rails.env}:jobs".freeze
-end
+module Elasticsearch; end
+
+es_config = (YAML.load_file("#{Rails.root}/config/elasticsearch.yml") || {})[Rails.env]
+
+Tire::Configuration.url(es_config['url']) if es_config && es_config['url'].present?
+
+Elasticsearch::INDEX_NAME = es_config && es_config['index_name'].present? ? es_config['index_name'].freeze : "#{Rails.env}:jobs".freeze
