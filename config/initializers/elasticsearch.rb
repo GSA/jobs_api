16b9cae28191daa2b7e8c3fd36136f9be98210e4
Yih En Lim <yihen@rrsoft.co>
2018-01-11 16:08:01 +0800

    Remove unnecessary rails config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,13 +4,13 @@ config = Rails.application.config.elasticsearch
 
 Elasticsearch::INDEX_NAME = config && config['index_name'].present? ? config['index_name'].freeze : "#{Rails.env}:jobs".freeze
 
-Rails.application.config.elasticsearch_client = Elasticsearch::Client.new(
+elasticsearch_client = Elasticsearch::Client.new(
   url: config['url'],
   user: config['username'],
   password: config['password']
 )
 
-Elasticsearch::Model.client = Rails.application.config.elasticsearch_client
+Elasticsearch::Model.client = elasticsearch_client
 
 PositionOpening.create_search_index unless PositionOpening.search_index_exists?
 Geoname.create_search_index unless Geoname.search_index_exists?
